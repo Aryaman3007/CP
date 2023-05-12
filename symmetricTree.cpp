d0102fe95cad4ce0654cf299067e9a23376145c4
Aryaman Singh <aryaman3007@gmail.com>
2023-05-12 13:27:23 +0530

    Create symmetricTree.cpp

---
+++ symmetricTree.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+bool solve(TreeNode *left, TreeNode *right)
+{
+    if (left == NULL || right == NULL)
+        return left == right;
+    if (left->val != right->val)
+        return false;
+    return solve(left->left, right->right) && solve(left->right, right->left);
+}
+
+bool isSymmetric(TreeNode *root)
+{
+    return (root == NULL) || solve(root->left, root->right);
+}
