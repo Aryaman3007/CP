b9f2c0629133a669e0af39b53ab899807ca00fc8
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2023-08-03 14:13:36 +0530

    Update detectCycleDirectedGraph.cpp

--- detectCycleDirectedGraph.cpp
+++ detectCycleDirectedGraph.cpp
@@ -37,4 +37,39 @@ bool isCyclic(int V, vector<int> adj[])
         }
     }
     return false;
+
+    // Using BFS
+
+    queue<int> q;
+    vector<int> ans;
+    int indegree[V] = {0};
+    for (int i = 0; i < V; i++)
+    {
+        for (auto it : adj[i])
+        {
+            indegree[it]++;
+        }
+    }
+
+    for (int i = 0; i < V; i++)
+    {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        for (auto it : adj[node])
+        {
+            indegree[it]--;
+            if (indegree[it] == 0)
+                q.push(it);
+        }
+        ans.push_back(node);
+    }
+    if (ans.size() == V)
+        return false;
+    return true;
 }
